37b57a703d2c1c0a07c57009fba72408ab6ab49d
RThemian <62709684+RThemian@users.noreply.github.com>
2022-06-20 09:45:02 -0600

    works at grabbing temperatures

--- src/App.js
+++ src/App.js
@@ -1,208 +1,78 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import {Form, Button} from 'react-bootstrap';
 import "./App.css";
-import { Button, Card, Form, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 
+export default function App() {
 
-//span element passes through text some attribute
-
-
-function Todo({ todo, text, index, markTodo, removeTodo, editTodo }) {
-  function getLocation() {
-    return;
-  }
-
-  return (
-    <div
-      className="todo"
-      
-    >
-      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
-      <div>
-        <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
-        <Button variant ='outline-warning' onClick={() => editTodo(index)}>✍️</Button>{' '}
-        <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>{' '}
-        <Button variant = "outline-info" onClick ={getLocation}>⛈</Button>
-
-        
-      </div>
-    </div>
-  );
-}
-
-function FormTodo({ addTodo }) {
-  const [value, setValue] = React.useState("");
-
-  
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-   
-    setValue("");
-  };
+  const [value, setValue] = useState('');
+  const [data, setData] = useState('');
+  const [temp, setTemp] = useState('');
+  const [location, setLocation] = useState("")
+ // const url = `http://api.weatherapi.com/v1/current.json?key=a389c7cedf1e4ac494e140828220806&q=${cityInput}&aqi=yes`
 
 
-
-  return (
-    <Form onSubmit={handleSubmit}> 
-    <Form.Group>
-      <Form.Label><b>Add Todo</b></Form.Label>
-
-      <Form.Control 
-        type="text" 
-        className="input" 
-        value={value} 
-        onChange={e => setValue(e.target.value)} 
-        placeholder="Add new todo" 
-      />
-
-    </Form.Group>
-    <Button variant="primary mb-3" type="submit">
-      Submit
-    </Button>
-  </Form>
-  );
-}
-
-function App() {
-
-  const [temp, setTemp] = React.useState('');
-  const [location, setLocation] = React.useState('london');
-
-  const getLocation = (text) => {
-    setLocation(text);
-  }
-
- React.useEffect(() => {
-
-  fetch(`http://api.weatherapi.com/v1/current.json?key=a389c7cedf1e4ac494e140828220806&q=${location}&aqi=no`)
-     .then(response => response.json())
-     .then(data => {
-       console.log(data)
-       console.log(data.current.temp_f)
-       setTemp(data.current.temp_f)
-       setLocation(data.location.name)
-     })
-     .catch(error => console.log(error))
-
-   }, [])
-
-
-
-
-  const [todos, setTodos] = React.useState([
-    {
-      text: "Where do you want to go?",
-      isDone: false
-    }
-  ]);
-
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
-
-  const markTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isDone = !newTodos[index].isDone;
-    setTodos(newTodos);
-    console.log("🐸🐸🐸", index);
-  };
-
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-    console.log("🐸", index);
-  };
-  const [modalIndex, setModalIndex] = React.useState(0);
-  const [edit, setEdit] = React.useState('')
-  const editTodo = index => {
-    console.log("🐸🐸", index);
-    setModalIndex(index);
-    setOpen(true);
-
-    const newTodos = [...todos];
-    //newTodos.splice(index, 1);
-    //I give up for today, here I can't figure out how to edit on my own, so i'll copy and paste
-    setTodos(newTodos)
-    
-    
-  }
-
-
-
-
-  const [open, setOpen] = React.useState(false);
-  console.log('🍎', modalIndex); 
-
-  
-
-  return (
-
-    <>
  
-    {/* <div>
-      <h1>I'm a modal</h1>
-      <button onClick = {e => setOpen(false)}></button>
-    </div> */}
+  // useEffect(() => {
+  // fetch(`http://api.weatherapi.com/v1/current.json?key=a389c7cedf1e4ac494e140828220806&q=${location}&aqi=no`)
+  // .then(response => response.json())
+  // .then(data => {
+  // console.log(data)
+  // console.log(data.current.temp_f)
+  // setTemp(data.current.temp_f)
 
-      <div className = "app">
+  //  })
+  //    .catch(error => console.error(error))
+
+  //  }, [])
+   
   
-      <div className = "container">
-     
-        <h1 className="text-center mb-4">Travel Todo List</h1>
-        <FormTodo addTodo={addTodo} />
-        <div>
-          {todos.map((todo, index) => (
-            <Card>
-              <Card.Body>
-                <Todo
-                key={index}
-                index={index}
-                todo={todo}
-                markTodo={markTodo}
-                removeTodo={removeTodo}
-                editTodo ={editTodo}
-                
-                />
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
-        
-        <Modal
-         show = {open}
-         onOpen = {e => setOpen(true)}
-         onClose = {e => setOpen(false)}
-         className="text-center mb-4"
-        > 
-      <Modal.Title  >Edit Todo</Modal.Title>
-      <Modal.Body>
-      {todos[modalIndex].text.toString() ? todos[modalIndex].text.toString() : "What now?" }
-    <Form > 
-    <Form.Group>
-      <Form.Label><b>change your todo</b></Form.Label>
-      <Form.Control  type="text" className="input" value={edit} onChange={e => setEdit(e.target.value)} placeholder={todos[modalIndex].text.toString() ? todos[modalIndex].text.toString() : "What now?" } />
-    </Form.Group>
-  </Form>
-    
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant = 'warning' onClick = {e => setOpen(false)} >Cancel</Button>
-        <Button variant = 'success' onClick = {e => {addTodo(edit)} }>Save and Close Todo</Button>
-        
-      </Modal.Footer>
 
-    </Modal>
-      </div>
-    </div>
+   //target the input using the event.key
+  const searchLocation = (event) => {
+    
+      console.log(location);
+    
+    //check is "enter" was hit on the keyboard
+     
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=a389c7cedf1e4ac494e140828220806&q=${location}&aqi=yes`)
+    .then((response) => {
+    setData(response.data)
+    console.log("🌿🍵💚🌱", response.data)
+    setTemp(response.data.current.temp_f)
+    })
+    .catch((error) => console.error(error))
+  }
+
+
+
+
+  return (
+    <>
+    <div className="container mt-2">
+      <h1 className="text-white bg-dark w-75">Hello World! {temp}</h1>
+      
+      
+    
+    <Form  onSubmit={searchLocation}>
+  <Form.Group className="mb-3 w-50" controlId="inputLocationWeather">
+    <Form.Label>Check the weather</Form.Label>
+    <Form.Control value = {location} onChange = {e => setLocation(e.target.value)} type="text" placeholder="Enter location to check weather" onKeyPress = {searchLocation} />
+    <Form.Text className="text-muted">
+      Enter where you would like to visit
+    </Form.Text>
+  </Form.Group>
+
+  <Button variant="primary" type="submit" onClick = {searchLocation}>
+    Submit
+  </Button>
+</Form>
+
+</div>
+
     </>
   );
-}
 
-export default App;
+};
